071d7a8605264d769f3aabc4920b5b3b3fd3987f
Roushan Kumar <roushan@Roushans-MacBook-Pro.local>
2025-02-15 01:13:56 +0530

    Added parcel

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-React App
+# React App 🚀
+
+
+# Parcel does these things ([text](https://parceljs.org/))
+- Dev build
+- local server
+- HMR (hot module replacement)
+- It uses a file watching algorithms
+- Caching - faster build
+- Image optimization
+- minification
+- Bundling
+- Compress the file
+- Content hashing
+- Code Splitting
+- Differential Bundling (Support older browser)
+- Diagnostic
+- Error handling
+- Https server can also be used
--- app.js
+++ app.js
@@ -1,10 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 // Nested elements creation
 const parent = React.createElement(
     "div", {id: "parent"}, React.createElement(
         "div", {id:"child"}, [React.createElement(
-            "h1", {}, "Nested h1 tag"),
-        React.createElement("h1", {}, "Nested h2 tag")]));
+            "h1", {}, "My App 🚀"),
+        React.createElement("h1", {}, "This is a learning App")]));
 
 const heading = React.createElement("h1", {id: "heading"}, "Hello world from React!");
 const root = ReactDOM.createRoot(document.getElementById("root"));
